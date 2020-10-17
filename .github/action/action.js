ccd2a35fbf87914038b3cdc71e71996332ec323e
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-10-18 09:45:48 +1100

    Update action.js

--- .github/action/action.js
+++ .github/action/action.js
@@ -1,11 +1,11 @@
-const fs = require("fs")
+const fs = require("fs");
 
 const badgeName = file => file.substring(0, file.lastIndexOf("."));
-const rawURL = file => `https://raw.githubusercontent.com/mudlabs/shields.io.endpoint/badges/${file}`;
+const rawURL = file => `https://raw.githubusercontent.com/mudlabs/shieldsio/endpoint/badges/${file}`;
 
 const toBadgeBlock = (string, filename) => {
   const badge_name = badgeName(filename);
-  const badge = `> [![${name}]](./${filename})<br/>`;
+  const badge = `> [![${badge_name}]](./${filename})<br/>`;
   const name = `> \`${badge_name}\`<br/>`;
   const file = `> [${filename}](./${filename})<br/><br/>`;
   return string += `${badge}${name}${file}\n`;
@@ -22,12 +22,12 @@ const toBadgeMarkdown = (string, filename) => {
     const template = await fs.promises.readFile(".github/action/template.md", { encoding: "utf-8" });
     const all_files = await fs.promises.readdir("./");
     const files = all_files.filter(file => file.split(".").pop() === "json");
-    const readme = template.replace(/\{\{(?:[a-z]|\.)+\}\}/g, match => {
+    const readme = template.replace(/\{\{badge\.(?:table|markdown)\}\}/g, match => {
       switch (match) {
         case "{{badges.table}}":
           return files.reduce(toBadgeBlock);
         case "{{badges.markdown}}":
-          return files.reduce(toBadgeMarkdown, "");
+          return files.reduce(toBadgeMarkdown);
       }
     });
     
