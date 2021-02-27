7459d7a53d3a38b14fe9361c0381a3e02dc20e65
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2021-02-27 12:08:39 +1100

    Update action.js

--- .github/action/action.js
+++ .github/action/action.js
@@ -4,7 +4,6 @@ const badgeName = file => file.substring(0, file.lastIndexOf("."));
 const rawURL = file => `https://raw.githubusercontent.com/mudlabs/shieldsio/endpoint/badges/${file}`;
 
 const toBadgeBlock = (string, filename) => {
-  console.log(string, filename)
   const badge_name = badgeName(filename);
   const badge = `> [![${badge_name}]](./${filename})<br/>`;
   const name = `> \`${badge_name}\`<br/>`;
@@ -23,7 +22,7 @@ const toBadgeMarkdown = (string, filename) => {
     const template = await fs.promises.readFile(".github/action/template.md", { encoding: "utf-8" });
     const all_files = await fs.promises.readdir("./badges/");
     const files = all_files.filter(file => file.split(".").pop() === "json");
-    const readme = template.replace(/\{\{badge\.(?:table|markdown)\}\}/g, match => {
+    const readme = template.replace(/\{\{badges\.(?:table|markdown)\}\}/g, match => {
       switch (match) {
         case "{{badges.table}}":
           return files.reduce(toBadgeBlock);
